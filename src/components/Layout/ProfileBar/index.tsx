bc4c5f1267aef2b863da1be7ffa2127be2aac384
Lucas Feitosa Alves <lukasalves271@gmail.com>
2022-11-11 22:05:57 -0300

    add profilebar

--- src/components/Layout/ProfileBar/index.tsx
+++ src/components/Layout/ProfileBar/index.tsx
@@ -4,19 +4,6 @@ import * as S from './styles'
 export function ProfileBar() {
   const [openMenu, setOpenMenu] = useState(false);
 
-  // let show = true;
-
-  // const menuSection = document.querySelector(".menu-section")
-  // const menuToggle = menuSection.querySelector(".menu-toggle")
-
-  // menuToggle.addEventListener("click", () => {
-
-  //   document.body.style.overflow = show ? "hidden" : "initial"
-
-  //   menuSection.classList.toggle("on", show)
-  //   show = !show;
-  // })
-
   return (
     <S.Container>
       <S.LogoContainer>
@@ -27,15 +14,12 @@ export function ProfileBar() {
         <S.ImageProfileContainer>
           <S.ImageProfile src="/assets/img/profile.png" alt="" />
         </S.ImageProfileContainer>
-        {/* menu-section */}
         <S.HamburguerMenuContainer open={openMenu} onClick={() => setOpenMenu(prev => !prev)}>
-          {/* menu-toggle */}
           <S.Hamburguer open={openMenu} onClick={() => setOpenMenu(prev => !prev)}>
             <div className="one"></div>
             <div className="two"></div>
             <div className="three"></div>
           </S.Hamburguer>
-          {/* nav */}
           <S.HamburguerMenu open={openMenu} onClick={() => setOpenMenu(prev => !prev)}>
             <ul>
               <li>
